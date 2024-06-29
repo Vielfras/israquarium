683cb622cdc805d3a03e60bfe9d23bf0c9176d34
vielfras <vielfras@gmail.com>
2024-06-29 23:05:01 +0300

    init commit

---
+++ client/README.md
@@ -0,0 +1,7 @@
+# Israquarium
+
+
+## Version 0.1.0
+- [ ] Add Tailwind
+- [ ] Create a Fish Display Component
+- [ ] Fetch data to display from server.
---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p>
+          Israquarium
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App;
