daf0c50e8fa7543844bf1b2aa1e43436e9cbf3ce
vielfras <vielfras@gmail.com>
2024-07-05 10:55:09 +0300

    added two basic fish routes

--- server/controllers/fishControllers.js
+++ server/controllers/fishControllers.js
@@ -1,29 +1,45 @@
 // usersControllers.js
 
 const schemas = require("../schemas/fishSchemas");
-const User = require("../models/User");
+// const User = require("../models/User");
+const Fish = require("../models/Fish");
 const Err = require("../utils/errorHandling");
 const Auth = require("../utils/authorisation");
 
 
 const getRandomFish = async (req, res) => {
   try {
-    
+    const count = await Fish.countDocuments();
+    const random = Math.floor(Math.random() * count);
+    const fish = await Fish.findOne().skip(random);
+    if (fish) {
+      return res.status(200).json({ success: true, data: fish });
+    }
+    return res.status(404).json({ success: false, 
+      message: "No fish found." 
+    });
   } catch (err) {
-    return res.status(400).json({ success: false, message: err.message, });
+    return res.status(400).json({ success: false, message: err.message });
   }
 };
-
 
 const getFishById = async (req, res) => {
   try {
-    
+    const { id } = req.params;
+
+    const fish = await Fish.findById(id);
+    if (fish) {
+      return res.status(200).json({ success: true, data: fish });
+    }
+
+    return res.status(404).json({
+      success: false,
+      message: `Fish id '${id}' not found.`,
+    });
   } catch (err) {
-    return res.status(400).json({ success: false, message: err.message, });
+    return res.status(400).json({ success: false, message: err.message });
   }
 };
-
-
 
 
 module.exports = {
