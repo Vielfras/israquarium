98288b269d6e75092285759cfaadd4745c40b250
vielfras <vielfras@gmail.com>
2024-07-11 21:52:36 +0300

    added schema validation options

--- server/schemas/commonSchemas.js
+++ server/schemas/commonSchemas.js
@@ -2,13 +2,18 @@
 
 const Joi = require('joi');
 
+const validationOptions = {
+    stripUnknown: true,
+    abortEarly: false,
+};
+
 
 const imageSchema = Joi.object({
     src: Joi.string().required(),
     alt: Joi.string().required(),
     creatorName: Joi.string().optional(),
     originalSource: Joi.string().optional(),
-});
+}).options(validationOptions);
 
 
 module.exports = { imageSchema, };
--- server/schemas/fishIndexSchemas.js
+++ server/schemas/fishIndexSchemas.js
@@ -4,12 +4,18 @@ const Joi = require('joi');
 const { imageSchema } = require('./commonSchemas');
 
 
+const validationOptions = {
+    stripUnknown: true,
+    abortEarly: false,
+};
+
+
 const fishIndexSchema = Joi.object({
     hebrew: Joi.string().required(),
     english: Joi.string().required(),
     russian: Joi.string().required(),
     image: imageSchema.optional(),
-});
+}).options(validationOptions);
 
 
 module.exports = { fishIndexSchema, };
