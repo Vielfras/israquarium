d734bde8b3a9dd0bdc243172a0217103e22c05d5
vielfras <vielfras@gmail.com>
2024-09-02 20:49:50 +0300

    Added a simple footer

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -6,86 +6,33 @@ import { FaFacebook, FaInstagram, FaLinkedinIn, FaTwitter } from 'react-icons/fa
 export default function Footer() {
 
   return (
-    <div className='Footer'>
-      <footer className='border-top pt-4'>
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-xl-3 col-lg-4 col-md-6">
-              <div>
-                <a href="#" className="navbar-brand">
-                  <BiSolidCircle size={23} color='red' />
-                  <BiSolidCircle size={23} color='orange' style={{ marginLeft: '-12px', opacity: '0.7' }} />
-                  <span className='pt-5' style={{ fontWeight: '500', fontFamily: 'monospace', fontSize: '1rem' }}>BizCard</span>
-                </a>
-                <p className="mb-30 footer-desc pt-2">
-                  We know it's tough, BizCard is a company dedicated to helping small business owners just like you connect with the right clients.
-                </p>
-                <p>
-                  Copyright &copy; BizCard, {new Date().getFullYear().toString()} All rights reserved.
-                </p>
-              </div>
-            </div>
-            <div className="col-xl-3 col-lg-3 col-md-6">
-              <div>
-                <h4>Services</h4>
-                <ul className="list-unstyled">
-                  <li>
-                    <a href="#" className="text-decoration-none">Marketing</a>
-                  </li>
-                  <li>
-                    <a href="#" className="text-decoration-none">Branding</a>
-                  </li>
-                  <li>
-                    <a href="#" className="text-decoration-none">Web Design</a>
-                  </li>
-                  <li>
-                    <a href="#" className="text-decoration-none">Graphics Design</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className="col-xl-2 col-lg-2 col-md-6">
-              <div>
-                <h4>Social Media</h4>
-                <ul className="list-unstyled">
-                  <li>
-                    <a href="#" className="text-decoration-none">
-                      <span><FaTwitter size={15} className='SocialMediaIcon' />&nbsp;</span>
-                      Tweet us a tweet
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" className="text-decoration-none">
-                      <span><FaFacebook size={15} className='SocialMediaIcon' />&nbsp;</span>
-                      Visit our page
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" className="text-decoration-none">
-                      <span><FaInstagram size={15} className='SocialMediaIcon' />&nbsp;</span>
-                      It's getting reel
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" className="text-decoration-none">
-                      <span><FaLinkedinIn size={15} className='SocialMediaIcon' />&nbsp;</span>
-                      We should link !
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className="col-xl-3 col-lg-3 col-md-6 col-sm-8 mx-auto">
-              <div>
-                <h4>Newsletter</h4>
-                <div>
-                  <label htmlFor="Newsletter" className="form-label">Subscribe To Our Newsletter</label>
-                </div>
-              </div>
-            </div>
-          </div>
+    <footer className="bg-white shadow dark:bg-gray-900">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+            <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Israquarium</span>
+          </a>
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">About</a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline">Contact</a>
+            </li>
+          </ul>
         </div>
-      </footer>
-    </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://flowbite.com/" className="hover:underline">Israquarium</a>. All Rights Reserved.</span>
+      </div>
+    </footer>
+
+
   )
 }
