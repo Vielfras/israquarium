80a2ec23798c09829395e2dfaee68d394aa39222
vielfras <vielfras@gmail.com>
2024-09-06 22:18:15 +0300

    added directionality to the text in About page

--- client/src/pages/About/About.tsx
+++ client/src/pages/About/About.tsx
@@ -1,3 +1,4 @@
+import { DirectionProvider } from '../../context/ReadingDirectionContext';
 import './About.scss';
 
 import { useTranslation } from 'react-i18next';
@@ -8,11 +9,14 @@ export default function About() {
   return (
     <div className="flex flex-col items-center justify-centertext-gray-900 dark:text-white">
       <h1 className="text-4xl font-bold m-6">{t('AboutPage.title')}</h1>
-      <div className="max-w-4xl mx-auto p-6">
-        <p className="text-lg mb-4">{t('AboutPage.description1')}</p>
-        <p className="text-lg mb-4">{t('AboutPage.description2')}</p>
-        <p className="text-lg">{t('AboutPage.description3')}</p>
-      </div>
+
+      <DirectionProvider>
+        <div className="max-w-4xl mx-auto p-6">
+          <p className="text-lg mb-4">{t('AboutPage.description1')}</p>
+          <p className="text-lg mb-4">{t('AboutPage.description2')}</p>
+          <p className="text-lg">{t('AboutPage.description3')}</p>
+        </div>
+      </DirectionProvider>
     </div>
   );
 }
