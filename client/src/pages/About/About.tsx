a3b89fe2aba1bf33fed077d8c5102fba6db2d0ee
vielfras <vielfras@gmail.com>
2024-09-06 22:16:11 +0300

    added a simple about page

--- client/src/pages/About/About.tsx
+++ client/src/pages/About/About.tsx
@@ -1,9 +1,18 @@
 import './About.scss';
 
+import { useTranslation } from 'react-i18next';
+
 export default function About() {
+  const { t } = useTranslation();
+
   return (
-    <div className="flex flex-col items-center justify-center h-full text-center">
-      <h1 className="text-6xl font-bold">About</h1>
+    <div className="flex flex-col items-center justify-centertext-gray-900 dark:text-white">
+      <h1 className="text-4xl font-bold m-6">{t('AboutPage.title')}</h1>
+      <div className="max-w-4xl mx-auto p-6">
+        <p className="text-lg mb-4">{t('AboutPage.description1')}</p>
+        <p className="text-lg mb-4">{t('AboutPage.description2')}</p>
+        <p className="text-lg">{t('AboutPage.description3')}</p>
+      </div>
     </div>
   );
 }
