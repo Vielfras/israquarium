4c634c59baa2348e8e3a45f173ef9e408a1305fc
vielfras <vielfras@gmail.com>
2024-09-06 10:48:03 +0300

    fixed spacing in ltr

--- client/src/components/Header/NavList/NavList.tsx
+++ client/src/components/Header/NavList/NavList.tsx
@@ -11,10 +11,7 @@ export default function NavList() {
     const isActive = (path: string) => location.pathname === path;
 
     return (
-        <ul
-            className={`flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:bg-white md:flex-row ${direction === 'rtl' ? 'space-x-reverse' : 'space-x-8'
-                } md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700`}
-        >
+        <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:bg-white md:flex-row gap-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
                 <Link to="/"
                     className={`block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0 ${isActive('/') ? 'text-blue-700 dark:text-blue-500 font-bold' : 'text-gray-900 dark:text-white'}`}>
