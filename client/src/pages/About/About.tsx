ec9f468deb217360e85d53acc8b24a62796038ea
vielfras <vielfras@gmail.com>
2024-09-08 22:23:23 +0300

    added a background to fit the rest of te websites style

--- client/src/pages/About/About.tsx
+++ client/src/pages/About/About.tsx
@@ -8,16 +8,18 @@ export default function About() {
   const { t } = useTranslation();
 
   return (
-    <div className="flex flex-col items-center justify-centertext-gray-900 dark:text-white">
-      <h1 className="text-4xl font-bold m-6">{t('AboutPage.title')}</h1>
+    <div className="flex justify-center items-center">
+      <div className="flex flex-col items-center rounded-lg justify-centertext-gray-900 dark:text-white bg-white dark:bg-gray-900">
+        <h1 className="text-4xl font-bold m-6">{t('AboutPage.title')}</h1>
 
-      <DirectionProvider>
-        <div className="max-w-4xl mx-auto p-6">
-          <p className="text-lg mb-4">{t('AboutPage.description1')}</p>
-          <p className="text-lg mb-4">{t('AboutPage.description2')}</p>
-          <p className="text-lg">{t('AboutPage.description3')}</p>
-        </div>
-      </DirectionProvider>
+        <DirectionProvider>
+          <div className="max-w-4xl mx-auto p-6">
+            <p className="text-lg mb-4">{t('AboutPage.description1')}</p>
+            <p className="text-lg mb-4">{t('AboutPage.description2')}</p>
+            <p className="text-lg">{t('AboutPage.description3')}</p>
+          </div>
+        </DirectionProvider>
+      </div>
     </div>
   );
 }
