fc7ab88a4d1fe2489238d6eb08191850b4d1d3bc
vielfras <vielfras@gmail.com>
2024-09-08 19:17:15 +0300

    Added localisation to sign up page

--- client/src/pages/SignUp/SignUp.tsx
+++ client/src/pages/SignUp/SignUp.tsx
@@ -1,11 +1,13 @@
-import './SignUp.css'
+import './SignUp.css';
 import { useContext, useState } from 'react';
 import { AuthContext } from '../../context/AuthContext';
 import { ToastsContext } from '../../context/ToastsContext';
 import { useNavigate } from 'react-router-dom';
 import FormField from '../../components/Form/FormField/FormField';
+import { useTranslation } from 'react-i18next';
 
 export default function SignUp() {
+  const { t } = useTranslation(); // i18next hook for translations
   const [firstName, setFirstName] = useState<string>();
   const [middleName, setMiddleName] = useState<string>();
   const [lastName, setLastName] = useState<string>();
@@ -25,71 +27,46 @@ export default function SignUp() {
   const toasts = useContext(ToastsContext);
   const navigate = useNavigate();
 
-  const phoneRegex = /0[0-9]{1,2}\-?\s?[0-9]{3}\s?[0-9]{4}/;
-  const nameRegex = /^[A-Za-z]{2,}$/;
-  const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-  const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d.*\d.*\d.*\d)(?=.*[_*&^%$#@!-])[A-Za-z\d_*&^%$#@!-]{8,}$/;
-
-  const [phoneIsValid, setPhoneIsValid] = useState<boolean>(true);
-  const [firstNameIsValid, setFirstNameIsValid] = useState<boolean>(true);
-  const [middleNameIsValid, setMiddleNameIsValid] = useState<boolean>(true);
-  const [lastNameIsValid, setLastNameIsValid] = useState<boolean>(true);
-  const [emailIsValid, setEmailIsValid] = useState<boolean>(true);
-  const [passwordIsValid, setPasswordIsValid] = useState<boolean>(true);
-  const [passwordVerificationIsValid, setPasswordVerificationIsValid] = useState<boolean>(true);
-
   const handleFirstNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setFirstName(value);
-    setFirstNameIsValid(nameRegex.test(value));
   };
 
   const handleMiddleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setMiddleName(value);
-    setMiddleNameIsValid(value === '' || nameRegex.test(value));
   };
 
   const handleLastNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setLastName(value);
-    setLastNameIsValid(nameRegex.test(value));
   };
 
   const handlePhoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setPhone(value);
-    setPhoneIsValid(phoneRegex.test(value));
   };
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
-    setEmailIsValid(emailRegex.test(e.target.value));
   };
 
   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     setPassword(value);
-    setPasswordIsValid(passwordRegex.test(value));
   };
 
   const handlePasswordVerificationChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     setPasswordVerification(value);
-    setPasswordVerificationIsValid(value === password);
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setIsBusy(true);
 
-    if (!auth) {
-      setIsBusy(false);
-      return;
-    }
-
-    if (!firstName || !lastName || !email || !password || !passwordVerification || password !== passwordVerification) {
-      toasts?.addToast('⚠️', 'Error Signing-Up', 'Please fill out all required fields.', 'danger');
+    if (!firstName || !lastName || !email || !password || password !== passwordVerification) {
+      toasts?.addToast('SignUpPage.⚠️', t('SignUpPage.signUpErrorTitle'), t('SignUpPage.signUpErrorMessage'), 'danger');
       setIsBusy(false);
       return;
     }
@@ -103,10 +80,6 @@ export default function SignUp() {
       phone: phone || "",
       email: email,
       password: password,
-      image: {
-        url: 'https://lorempokemon.fakerapi.it/pokemon',
-        alt: 'You wrote that this is NOT required 😉',
-      },
       address: {
         country: country || "",
         city: city || "",
@@ -117,60 +90,46 @@ export default function SignUp() {
       isBusiness,
     };
 
-
     const { error } = await auth.signUp(userData);
     if (error) {
-      toasts?.addToast('⚠️', 'Error Signing-Up', error, 'danger');
+      toasts?.addToast('SignUpPage.⚠️', t('SignUpPage.signUpErrorTitle'), error, 'danger');
     } else {
-      toasts?.addToast('👍🏼', 'Successfully Signed-Up', 'Please sign in with your credentials.', 'success');
+      toasts?.addToast('SignUpPage.👍🏼', t('SignUpPage.signUpSuccessTitle'), t('SignUpPage.signUpSuccessMessage'), 'success');
       navigate('/signin');
     }
 
     setIsBusy(false);
   };
 
-
   return (
     <div className="SignUp Page flex justify-center items-center">
       <div className="bg-white dark:bg-gray-900 p-8 rounded-lg text-gray-900 dark:text-gray-50">
-        <h3 className="text-3xl font-bold mb-6 text-center">Sign-Up Page</h3>
-
+        <h3 className="text-3xl font-bold mb-6 text-center">{t('SignUpPage.signUpPageTitle')}</h3>
         <form onSubmit={handleSubmit} className="space-y-6">
-
-          {/* Full Name */}
           <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
             <FormField
               controlId="formGridFirstName"
-              label="First Name"
+              label={t('SignUpPage.firstNameLabel')}
               type="text"
-              placeholder="First"
+              placeholder={t('SignUpPage.firstNamePlaceholder')}
               value={firstName || ''}
               onChange={handleFirstNameChange}
-              regex={nameRegex}
-              validationMessage="First name must be at least 2 letters."
-              isValid={firstNameIsValid}
             />
             <FormField
               controlId="formGridMiddleName"
-              label="Middle Name (Optional)"
+              label={t('SignUpPage.middleNameLabel')}
               type="text"
-              placeholder="Middle"
+              placeholder={t('SignUpPage.middleNamePlaceholder')}
               value={middleName || ''}
               onChange={handleMiddleNameChange}
-              regex={nameRegex}
-              validationMessage="Middle name must be at least 2 letters."
-              isValid={middleNameIsValid}
             />
             <FormField
               controlId="formGridLastName"
-              label="Last Name"
+              label={t('SignUpPage.lastNameLabel')}
               type="text"
-              placeholder="Last"
+              placeholder={t('SignUpPage.lastNamePlaceholder')}
               value={lastName || ''}
               onChange={handleLastNameChange}
-              regex={nameRegex}
-              validationMessage="Last name must be at least 2 letters."
-              isValid={lastNameIsValid}
             />
           </div>
 
@@ -178,25 +137,19 @@ export default function SignUp() {
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
             <FormField
               controlId="formGridPhone"
-              label="Phone"
+              label={t('SignUpPage.phoneLabel')}
               type="text"
-              placeholder="Phone number"
+              placeholder={t('SignUpPage.phonePlaceholder')}
               value={phone || ''}
               onChange={handlePhoneChange}
-              regex={phoneRegex}
-              validationMessage="Please enter a valid phone number."
-              isValid={phoneIsValid}
             />
             <FormField
               controlId="formGridEmail"
-              label="Email"
+              label={t('SignUpPage.emailLabel')}
               type="email"
-              placeholder="Email Address"
+              placeholder={t('SignUpPage.emailPlaceholder')}
               value={email || ''}
               onChange={handleEmailChange}
-              regex={emailRegex}
-              validationMessage="Please enter a valid email address."
-              isValid={emailIsValid}
             />
           </div>
 
@@ -204,27 +157,20 @@ export default function SignUp() {
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
             <FormField
               controlId="formGridPassword"
-              label="Password"
+              label={t('SignUpPage.passwordLabel')}
               type="password"
-              placeholder="Password"
+              placeholder={t('SignUpPage.passwordPlaceholder')}
               value={password || ''}
               onChange={handlePasswordChange}
-              regex={passwordRegex}
-              validationMessage="Password must be at least 8 characters long, include 1 capital letter, 1 lowercase letter, at least 1 number, and 1 special character."
-              isValid={passwordIsValid}
             />
-            <div className="col-span-1">
-              <FormField
-                controlId="formGridPasswordVerification"
-                label="Password Verification"
-                type="password"
-                placeholder="Verify Password"
-                value={passwordVerification || ''}
-                onChange={handlePasswordVerificationChange}
-                validationMessage="Passwords must match."
-                isValid={passwordVerificationIsValid}
-              />
-            </div>
+            <FormField
+              controlId="formGridPasswordVerification"
+              label={t('SignUpPage.passwordVerificationLabel')}
+              type="password"
+              placeholder={t('SignUpPage.passwordVerificationPlaceholder')}
+              value={passwordVerification || ''}
+              onChange={handlePasswordVerificationChange}
+            />
           </div>
 
           <hr className="my-6 border-gray-300 dark:border-gray-700" />
@@ -233,17 +179,17 @@ export default function SignUp() {
           <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
             <FormField
               controlId="formGridCountry"
-              label="Country"
+              label={t('SignUpPage.countryLabel')}
               type="text"
-              placeholder="Country"
+              placeholder={t('SignUpPage.countryPlaceholder')}
               value={country || ''}
               onChange={(e) => setCountry(e.target.value)}
             />
             <FormField
               controlId="formGridCity"
-              label="City"
+              label={t('SignUpPage.cityLabel')}
               type="text"
-              placeholder="City"
+              placeholder={t('SignUpPage.cityPlaceholder')}
               value={city || ''}
               onChange={(e) => setCity(e.target.value)}
             />
@@ -252,25 +198,25 @@ export default function SignUp() {
           <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
             <FormField
               controlId="formGridStreet"
-              label="Street"
+              label={t('SignUpPage.streetLabel')}
               type="text"
-              placeholder="Street"
+              placeholder={t('SignUpPage.streetPlaceholder')}
               value={street || ''}
               onChange={(e) => setStreet(e.target.value)}
             />
             <FormField
               controlId="formGridHouseNumber"
-              label="House Number"
+              label={t('SignUpPage.houseNumberLabel')}
               type="text"
-              placeholder="House Number"
+              placeholder={t('SignUpPage.houseNumberPlaceholder')}
               value={houseNumber || ''}
               onChange={(e) => setHouseNumber(e.target.value)}
             />
             <FormField
               controlId="formGridZipCode"
-              label="Zip Code"
+              label={t('SignUpPage.zipCodeLabel')}
               type="text"
-              placeholder="Zip Code"
+              placeholder={t('SignUpPage.zipCodePlaceholder')}
               value={zipCode || ''}
               onChange={(e) => setZipCode(e.target.value)}
             />
@@ -278,9 +224,9 @@ export default function SignUp() {
 
           <hr className="my-6 border-gray-300 dark:border-gray-700" />
 
-          {/* Business Signup */}
+          {/* Business */}
           <div className="text-center">
-            <label className="block text-lg font-medium mb-3">Sign Up as a Business?</label>
+            <label className="block text-lg font-medium mb-3">{t('SignUpPage.businessSignupLabel')}</label>
             <div className="flex justify-center">
               <label className="inline-flex items-center">
                 <input
@@ -289,45 +235,23 @@ export default function SignUp() {
                   checked={isBusiness}
                   onChange={(e) => setIsBusiness(e.target.checked)}
                 />
-                <span className="ml-2">Yes</span>
+                <span className="ml-2">{t('SignUpPage.yesLabel')}</span>
               </label>
             </div>
           </div>
 
           <hr className="my-6 border-gray-300 dark:border-gray-700" />
 
-          {/* Submit */}
           <div className="text-center">
             <button
               type="submit"
-              className={`bg-indigo-600 text-white py-2 px-4 rounded-lg shadow hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 ${isBusy ? 'opacity-50' : ''
-                }`}
+              className={`bg-indigo-600 text-white py-2 px-4 rounded-lg shadow hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 ${isBusy ? 'opacity-50' : ''}`}
               disabled={isBusy}
             >
-              {isBusy ? (
-                <svg
-                  className="animate-spin h-5 w-5 mx-auto text-white"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <circle
-                    className="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    strokeWidth="4"
-                  ></circle>
-                  <path
-                    className="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8v4a4 4 0 100 8V4z"
-                  ></path>
-                </svg>
-              ) : (
-                'Sign Up'
-              )}
+              {isBusy ? <svg className="animate-spin h-5 w-5 mx-auto text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 100 8V4z"></path>
+              </svg> : t('SignUpPage.submitButton')}
             </button>
           </div>
         </form>
