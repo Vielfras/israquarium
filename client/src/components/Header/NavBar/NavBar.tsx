f927693be59e59baba95c460ea246a31c6be33b1
vielfras <vielfras@gmail.com>
2024-09-09 22:29:47 +0300

    Fixed logo routing

--- client/src/components/Header/NavBar/NavBar.tsx
+++ client/src/components/Header/NavBar/NavBar.tsx
@@ -7,6 +7,7 @@ import DarkLightToggle from '../DarkLightToggle/DarkLightToggle';
 import { useTranslation } from 'react-i18next'; 
 import { DirectionProvider} from '../../../context/ReadingDirectionContext';
 import NavList from '../NavList/NavList';
+import { Link } from 'react-router-dom';
 
 export default function NavBar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -19,12 +20,12 @@ export default function NavBar() {
     return (
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
-                <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
                     <span className="hidden lg:block self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
                         Israquarium
                     </span>
-                </a>
+                </Link>
                 <div className="flex items-center md:hidden">
                     <DarkLightToggle />
                     <UserMenu />
