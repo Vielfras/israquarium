4195cbe44de073911280b1e2c067a2b4e75a2643
vielfras <vielfras@gmail.com>
2024-09-16 22:33:36 +0300

    added clickOutside hook to user menu

--- client/src/components/Header/UserMenu/UserMenu.tsx
+++ client/src/components/Header/UserMenu/UserMenu.tsx
@@ -1,8 +1,11 @@
-import { useState, useContext } from 'react';
+// UserMenu.tsx
+
+import { useState, useContext, useRef } from 'react';
 import './UserMenu.scss';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../context/AuthContext';
 import { useTranslation } from 'react-i18next';
+import useClickOutside from '../../../hooks/useClickOutside';
 
 export default function UserMenu() {
   const auth = useContext(AuthContext);
@@ -10,7 +13,7 @@ export default function UserMenu() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
+    setIsDropdownOpen(prevState => !prevState);
   };
 
   const closeDropdown = () => {
@@ -25,22 +28,39 @@ export default function UserMenu() {
     }
   };
 
+  const handleClickOutside = () => {
+    if (isDropdownOpen) {
+      setIsDropdownOpen(false);
+    }
+  };
+  
+  const containerRef = useRef<HTMLDivElement>(null);
+  useClickOutside(containerRef, handleClickOutside);
+
   return (
-    <div className="relative flex items-center space-x-3 rtl:space-x-reverse">
-      <button type="button" onClick={toggleDropdown} id="user-menu-button"
+    <div className="relative flex items-center space-x-3 rtl:space-x-reverse" ref={containerRef}>
+      <button
+        type="button"
+        onClick={toggleDropdown}
+        id="user-menu-button"
         className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-        aria-expanded={isDropdownOpen ? "true" : "false"} aria-controls="user-dropdown">
+        aria-expanded={isDropdownOpen ? "true" : "false"}
+        aria-controls="user-dropdown"
+      >
         <span className="sr-only">{t('UserMenu.defaultAltText')}</span>
-        <img className="w-8 h-8 rounded-full"
+        <img
+          className="w-8 h-8 rounded-full"
           src={auth?.userDetails?.image?.src || '/images/default_profile_svg.svg'}
-          alt={auth?.userDetails?.image?.alt || t('UserMenu.defaultAltText')} />
+          alt={auth?.userDetails?.image?.alt || t('UserMenu.defaultAltText')}
+        />
       </button>
 
       {isDropdownOpen && (
         <div
           className="absolute right-0 z-50 mt-2 w-48 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
           id="user-dropdown"
-          style={{ top: '50%' }}>
+          style={{ top: '50%' }}
+        >
           <div className="px-4 py-3">
             {auth?.userDetails ? (
               <>
@@ -61,23 +81,31 @@ export default function UserMenu() {
             {auth?.userDetails ? (
               <>
                 <li>
-                  <Link to="/my-aquarium" onClick={closeDropdown}
+                  <Link
+                    to="/my-aquarium"
+                    onClick={closeDropdown}
                     className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
-                    role="menuitem">
+                    role="menuitem"
+                  >
                     {t('UserMenu.myAquarium')}
                   </Link>
                 </li>
                 <li>
-                  <Link to="/user-profile" onClick={closeDropdown}
+                  <Link
+                    to="/user-profile"
+                    onClick={closeDropdown}
                     className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
-                    role="menuitem">
+                    role="menuitem"
+                  >
                     {t('UserMenu.myProfile')}
                   </Link>
                 </li>
                 <li>
-                  <button onClick={handleSignOut}
+                  <button
+                    onClick={handleSignOut}
                     className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
-                    role="menuitem">
+                    role="menuitem"
+                  >
                     {t('UserMenu.signOut')}
                   </button>
                 </li>
@@ -85,16 +113,22 @@ export default function UserMenu() {
             ) : (
               <>
                 <li>
-                  <Link to="/sign-in" onClick={closeDropdown}
+                  <Link
+                    to="/sign-in"
+                    onClick={closeDropdown}
                     className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
-                    role="menuitem">
+                    role="menuitem"
+                  >
                     {t('UserMenu.signIn')}
                   </Link>
                 </li>
                 <li>
-                  <Link to="/sign-up" onClick={closeDropdown}
+                  <Link
+                    to="/sign-up"
+                    onClick={closeDropdown}
                     className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
-                    role="menuitem">
+                    role="menuitem"
+                  >
                     {t('UserMenu.register')}
                   </Link>
                 </li>
